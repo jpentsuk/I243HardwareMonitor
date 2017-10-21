72b22f0bd44232297609ca28a98b776ff0951bd4
Joonas Ervald <joonas.ervald@gmail.com>
2017-10-21 23:23:21 +0300

    Add Component class to base hardware components and sensors from

---
+++ I243HardwareMonitor/Component.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I243HardwareMonitor
+{
+	public class Component
+	{
+		public String Name { get; }
+		public String Type { get; }
+		public Component(String name, String type)
+		{
+			this.Name = name;
+			this.Type = type;
+		}
+	}
+}
