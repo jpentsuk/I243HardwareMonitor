37e3c3704a46619d70f67f32fb03783e743d07d7
Joonas Ervald <joonas.ervald@gmail.com>
2017-10-21 23:23:55 +0300

    Add HardwareComponent class to map out hardware component sub hardware and sensors

---
+++ I243HardwareMonitor/HardwareComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I243HardwareMonitor
+{
+	public class HardwareComponent : Component
+	{
+		private List<HardwareComponent> SubHardware { get; }
+		private List<HardwareSensor> Sensors { get; }
+		public HardwareComponent(String name, String type) : base(name, type)
+		{
+			this.SubHardware = new List<HardwareComponent>();
+			this.Sensors = new List<HardwareSensor>();
+		}
+	}
+}
