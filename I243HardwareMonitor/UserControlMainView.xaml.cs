7b3de4bb34cf82858dcb5bb6b58f4c258f24bf4c
Joonas Ervald <joonas.ervald@gmail.com>
2017-11-09 21:13:33 +0200

    Add a progress bar to represent current loads & temperatures

--- I243HardwareMonitor/UserControlMainView.xaml.cs
+++ I243HardwareMonitor/UserControlMainView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +21,14 @@ namespace I243HardwareMonitor
 	/// </summary>
 	public partial class UserControlMainView : UserControl
 	{
-		private HardwareInfo hardware;
 		public ViewType type { get; }
+		private HardwareInfo hardware;
 		private List<HardwareComponent> components;
+		private List<ProgressBar> progressBars;
 		public UserControlMainView(HardwareInfo hardware, ViewType type)
 		{
 			this.components = new List<HardwareComponent>();
+			this.progressBars = new List<ProgressBar>();
 			this.type = type;
 			this.hardware = hardware;
 			setComponent();
@@ -33,7 +36,26 @@ namespace I243HardwareMonitor
 			foreach (HardwareComponent component in components)
 			{
 				componentName.Text = type + ": " + component.Name;
-			}	
+			}
+			ProgressBar loadProgressBar = new ProgressBar();
+			loadProgressBar.Margin = new Thickness(10, 0, 10, 5);
+			loadProgressBar.Visibility = Visibility.Visible;
+			loadProgressBar.Height = 25;
+			loadProgressBar.Foreground = System.Windows.Media.Brushes.Blue;
+			loadProgressBar.Background = System.Windows.Media.Brushes.White;
+			loadProgressBar.Maximum = 110;
+			loadProgressBar.Value = 0;
+			containerPanel.Children.Add(loadProgressBar);
+			progressBars.Add(loadProgressBar);
+			Debug.WriteLine(components[0].ToString());
+			if (type == ViewType.RAM)
+			{	
+				loadProgressBar.Maximum = Double.Parse(getSensorWithType("Available").Value);
+			} else if (type == ViewType.HDD)
+			{
+				loadProgressBar.Visibility = Visibility.Hidden;
+			}
+			
 		}
 
 		public void setComponent()
@@ -70,9 +92,50 @@ namespace I243HardwareMonitor
 			setComponent();
 			foreach (HardwareComponent component in components)
 			{
-				labelInfo += component + Environment.NewLine;
+				HardwareSensor mainSensor;
+				switch (type)
+				{
+					case ViewType.CPU:
+						progressBars[0].Value = double.Parse(component.Sensors[component.Sensors.Count - 1].Value);
+						labelInfo = "Total " + component.Sensors[component.Sensors.Count - 1].Type + ": " + component.Sensors[component.Sensors.Count - 1].Value;
+						break;
+					case ViewType.GPU:
+						mainSensor = getSensorWithType("Temperature");
+						labelInfo = mainSensor.Type + ": " + mainSensor.Value;
+						progressBars[0].Value = double.Parse(mainSensor.Value);
+						break;
+					case ViewType.HDD:
+						mainSensor = getSensorWithType("Used");
+						labelInfo = mainSensor.Type + ": " + mainSensor.Value;
+						progressBars[0].Value = double.Parse(mainSensor.Value);
+						break;
+					case ViewType.RAM:
+						mainSensor = getSensorWithType("Used");
+						labelInfo = "In use : " + mainSensor.Value;
+						progressBars[0].Value = double.Parse(mainSensor.Value);
+						break;
+				}
 			}
 			lbl_main_view_info.Content = labelInfo;
+		}
+
+		private HardwareSensor getSensorWithType(String type)
+		{		
+			foreach (HardwareComponent comp in components)
+			{
+				foreach (HardwareSensor sensor in comp.Sensors)
+				{
+					if (sensor.Type.Contains(type))
+					{
+						return sensor;
+					}
+					if (sensor.Name.Contains(type))
+					{
+						return sensor;
+					}
+				}
+			}
+			return new HardwareSensor("null", "null", "null");
 		}
 	}
 }
