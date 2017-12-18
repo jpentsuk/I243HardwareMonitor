793c92d0448c53e6d38d258a7febf0ee60126295
mpalmeos <maie.palmeos@itcollege.ee>
2017-12-18 21:04:38 +0200

    DelegateCommand moved to different class in preparation of MVVM implementation

---
+++ I243HardwareMonitor/ViewModels/DelegateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace I243HardwareMonitor.ViewModels
+{
+    public class DelegateCommand : ICommand
+    {        
+        public Action CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null || CanExecuteFunc();
+        }
+
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+    }
+}
--- I243HardwareMonitor/ViewModels/NotifyIconViewModel.cs
+++ I243HardwareMonitor/ViewModels/NotifyIconViewModel.cs
@@ -2,35 +2,12 @@
 using System.Windows;
 using System.Windows.Input;
 
-namespace I243HardwareMonitor
+namespace I243HardwareMonitor.ViewModels
 {
     //Solution for this class is largely based on example given in https://www.codeproject.com/Articles/36468/WPF-NotifyIcon
     //This is the suggested method by the maker of the NotifyIcon add-on used in this project.
     public class NotifyIconViewModel
-    {
-
-        public class DelegateCommand : ICommand
-        {
-            public Action CommandAction { get; set; }
-            public Func<bool> CanExecuteFunc { get; set; }
-
-            public event EventHandler CanExecuteChanged
-            {
-                add { CommandManager.RequerySuggested += value; }
-                remove { CommandManager.RequerySuggested -= value; }
-            }
-
-            public bool CanExecute(object parameter)
-            {
-                return CanExecuteFunc == null || CanExecuteFunc();
-            }
-
-            public void Execute(object parameter)
-            {
-                CommandAction();
-            }
-        }
-        
+    {   
         /// <summary>
         /// Shows a window, if there isn't one open already. ICommand is mandatory to use button commands.
         /// </summary>
